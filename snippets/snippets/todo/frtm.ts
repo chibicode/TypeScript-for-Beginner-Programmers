5e32d5c4c6a5308bbcb8927f148eb9e5c46fc0d6
Shu Uesugi <shu@chibicode.com>
2019-12-06 22:43:25 +0900

    Todo app 2 (#11)
    * Wordsmith
    
    * Recap

---
+++ snippets/snippets/todo/frtm.ts
@@ -0,0 +1,11 @@
+type Todo = {
+  id: number
+  text: string
+  done: boolean
+}
+
+// Make sure that the input and the output
+// is of the correct type
+function toggleTodo(todo: Todo): Todo {
+  // ...
+}
