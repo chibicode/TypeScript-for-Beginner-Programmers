d4909ba1a707f0d9a53824151d2995422240d007
Shu Uesugi <shu@chibicode.com>
2019-12-18 19:33:34 +0900

    Update og image

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -11,7 +11,7 @@ export const articlesData = {
       'TypeScript Tutorial for JS Programmers Who Know How to Build a Todo App',
     date: DateTime.fromISO('2019-12-18T12:00:00Z'),
     description: 'Learn TypeScript by Building a Todo App',
-    ogImage: 'todo'
+    ogImage: 'todo-v2'
   },
   generics: {
     title:
