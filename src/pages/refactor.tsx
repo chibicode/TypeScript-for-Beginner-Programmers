be70d31314c0810f889dc6e769a6b4911c4b4431
Shu Uesugi <shu@chibicode.com>
2019-12-24 08:42:31 -1000

    Start writing refactor article

--- src/pages/refactor.tsx
+++ src/pages/refactor.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PostPage from 'src/components/PostPage'
-import { P } from 'src/components/ContentTags'
+import { P, Highlight } from 'src/components/ContentTags'
 
 const Page = () => (
   <PostPage
@@ -8,10 +8,22 @@ const Page = () => (
     articleKey="refactor"
     cards={[
       {
-        title: <>Test</>,
         content: (
           <>
-            <P>Test</P>
+            <P>
+              You’ve probably come across a piece of code that’s hard to
+              understand. To make it more understandable, you need to{' '}
+              <strong>refactor</strong> the code. And many programmers have
+              written how-to guides on refactoring code.
+            </P>
+            <P>
+              <Highlight>
+                You’ve probably also come across a{' '}
+                <strong>coding tutorial</strong> that’s hard to understand.
+              </Highlight>{' '}
+              Maybe you wanted to learn a new language, library, or framework,
+              but the tutorial you Google’d made you more confused as ever.
+            </P>
           </>
         )
       }
