9aaef5260bc7089acf3f2a9ea2e605477b9a0a86
Shu Uesugi <shu@chibicode.com>
2019-12-24 20:48:01 -1000

    Wordsmith

--- src/pages/refactor.tsx
+++ src/pages/refactor.tsx
@@ -12,10 +12,9 @@ const Page = () => (
         content: (
           <>
             <P>
-              You’ve probably come across a piece of code that’s hard to
-              understand. To make it more understandable, you need to{' '}
-              <strong>refactor</strong> the code. And many programmers have
-              written how-to guides on refactoring code.
+              At some point in your coding career, you’ve probably come across a
+              piece of code that’s hard to understand. To make it more
+              understandable, you need to <strong>refactor</strong> the code.
             </P>
             <EmojiSeparator
               emojis={['uglyCode', 'refactorArrow', 'cleanCode']}
@@ -24,12 +23,13 @@ const Page = () => (
               }
             />
             <P>
+              Similarly,{' '}
               <Highlight>
-                You’ve probably also come across a{' '}
-                <strong>coding tutorial</strong> that’s hard to understand.
+                you’ve probably also come across a coding{' '}
+                <strong>tutorial</strong> that’s hard to understand.
               </Highlight>{' '}
               Maybe you wanted to learn a new language, library, or framework,
-              but the tutorial you Google’d made you more confused as ever.
+              but the tutorial you found made you more confused as ever.
             </P>
             <EmojiSeparator
               emojis={['question', 'cryingCat', 'question']}
@@ -40,6 +40,11 @@ const Page = () => (
                 </>
               }
             />
+            <P>
+              Those tutorials can also be refactored to ... While many
+              programmers have written how-to guides on refactoring code, but
+              ...
+            </P>
           </>
         )
       }
