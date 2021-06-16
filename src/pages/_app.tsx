b2c191a4bad803631a006ba94d63cbdef3c2687d
Shu Uesugi <shu@chibicode.com>
2021-06-15 23:56:40 -0700

    Remove twitter embed (#40)
    * Remove twitter embed
    
    * Remove reloadTwitterScript

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import App from 'next/app'
-import Router from 'next/router'
-import reloadTwitterScript from 'src/lib/reloadTwitterScript'
-
-Router.events.on('routeChangeStart', () => {
-  reloadTwitterScript()
-})
-
-export default App
